82d72aae23e642d6c6b6ffacbc46a6812183914c
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2025-09-19 23:11:00 +0530

    About data update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,20 +2,27 @@ import React from "react"
 
 export default function About() {
   return (
-    <section className="p-6 md:p-20 max-w-3xl mx-auto  rounded-xl ">
+    <section className="p-6 md:p-20 max-w-3xl mx-auto rounded-xl ">
       <h2 className="text-3xl font-bold mb-4 text-white">
         About Us
       </h2>
       <p className="leading-relaxed mb-4 text-white">
-        Jarurat Care is a modern patient management dashboard that helps doctors
-        and healthcare workers keep track of patient details, history, and
-        contact information. Designed with a clean, responsive UI and seamless
-        search features.
+        At the heart of care and compassion stands Hospital Jarurat,
+a place where healing meets hope.
+With dedicated doctors, skilled nurses, and modern facilities,
+it offers patients not just treatment, but comfort and dignity.
       </p>
       <p className="leading-relaxed mb-4 text-white">
-        This project is built using <strong>React</strong>, styled with{" "}
-        <strong>TailwindCSS</strong>, and enhanced with{" "}
-        <strong>Framer Motion</strong> for smooth animations.
+        Each ward carries a promise of safety,
+each smile reassures families in worry.
+From advanced technology to human touch,
+Jarurat ensures health is nurtured with trust.
+      </p>
+      <p className="leading-relaxed text-white">
+        More than a hospital, it is a beacon of service,
+where community and wellbeing go hand in hand.
+Here, every patient is valued, every life respected,
+and every tomorrow made brighter with care.
       </p>
     </section>
   );
